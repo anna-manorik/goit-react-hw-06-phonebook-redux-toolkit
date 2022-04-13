7dd0a3d987764b7f101f5d4f3fbe60e7bc4a1946
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-13 16:44:10 +0300

    Create Filter.js

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as actions from '../../redux/phonebook-action';
+import { connect } from 'react-redux';
+
+const Filter = ({ filterValue, changeFilter }) => (
+  <label>
+    Search:
+    <input type="text" value={filterValue} onChange={changeFilter} />
+  </label>
+);
+
+Filter.propTypes = {
+  filterValue: PropTypes.string,
+  changeFilter: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  filterValue: state.filterReduser,
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeFilter: e => dispatch(actions.changeFilter(e.target.value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
