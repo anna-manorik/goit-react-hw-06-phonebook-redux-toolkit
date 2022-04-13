78573999556c839d1c8a3e699108eb392a236c8a
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-13 23:47:08 +0300

    Update phonebook-reduser.js

--- src/redux/phonebook-reduser.js
+++ src/redux/phonebook-reduser.js
@@ -1,52 +1,44 @@
-import { createStore, combineReducers } from 'redux';
+import { configureStore, createReducer } from '@reduxjs/toolkit';
+import actions from './phonebook-action';
 import shortid from 'shortid';
 
-const contactReduser = (state = [], { type, payload }) => {
-  switch (type) {
-    case 'ADD_CONTACT': {
-      const checkname = state.find(contact =>
-        contact.name.toLowerCase().includes(payload.name.toLowerCase())
-      );
+const contactReduser = createReducer([], {
+  [actions.addContact]: (state, { payload }) => {
+    const checkname = state.find(contact =>
+      contact.name.toLowerCase().includes(payload.name.toLowerCase())
+    );
 
-      if (!checkname) {
-        const contact = {
-          id: shortid.generate(),
-          name: payload.name,
-          number: payload.number,
-        };
+    if (!checkname) {
+      const contact = {
+        id: shortid.generate(),
+        name: payload.name,
+        number: payload.number,
+      };
 
-        return [contact, ...state];
-      } else {
-        alert(payload.name + ' is already in contact list');
-      }
-
-      break;
+      return [contact, ...state];
+    } else {
+      alert(payload.name + ' is already in contact list');
     }
-
-    case 'DELETE_CONTACT': {
-      return state.filter(contact => contact.id !== payload);
-    }
-
-    default:
-      return state;
-  }
-};
-
-const filterReduser = (state = '', { type, payload }) => {
-  switch (type) {
-    case 'CHANGE_FILTER':
-      return payload;
-
-    default:
-      return state;
-  }
-};
-
-const rootReduser = combineReducers({
-  contactReduser,
-  filterReduser,
+  },
+  [actions.deleteContact]: (state, { payload }) => {
+    
+    return state.filter(contact => contact.id !== payload);
+  },
 });
 
-const store = createStore(rootReduser);
+const filterReduser = createReducer('', {
+  [actions.changeFilter]: (state, {payload}) => {
+    console.log('state filter', state);
+    console.log('state payload', payload);
+    return payload;
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    contactReduser,
+    filterReduser,
+  },
+});
 
 export default store;
