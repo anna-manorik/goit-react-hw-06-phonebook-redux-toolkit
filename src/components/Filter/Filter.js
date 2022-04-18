862c9fb1616aeddaac7b6e7def37396183d716aa
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-18 22:01:35 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import actions from '../../redux/phonebook-action';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Filter = ({ filterValue, changeFilter }) => (
-  <label>
-    Search:
-    <input type="text" value={filterValue} onChange={changeFilter} />
-  </label>
-);
+const Filter = () => {
+  const filterValue = useSelector(state => state.filterReduser);
+  const dispatch = useDispatch();
+
+  const changeFilter = e => dispatch(actions.changeFilter(e.target.value));
+
+  return (
+    <label>
+      Search:
+      <input type="text" value={filterValue} onChange={changeFilter} />
+    </label>
+  );
+};
 
 Filter.propTypes = {
   filterValue: PropTypes.string,
   changeFilter: PropTypes.func,
 };
 
-const mapStateToProps = state => ({
-  filterValue: state.filterReduser,
-});
+// const mapStateToProps = state => ({
+//   filterValue: state.filterReduser,
+// });
 
-const mapDispatchToProps = dispatch => ({
-  changeFilter: e => dispatch(actions.changeFilter(e.target.value)),
-});
+// const mapDispatchToProps = dispatch => ({
+//   changeFilter: e => dispatch(actions.changeFilter(e.target.value)),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default Filter;
